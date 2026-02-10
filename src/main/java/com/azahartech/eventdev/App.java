56deed119a4cdb7baf18fcb5694dd53cace42732
Cristian Garcia <cj.garciamarcos@edu.gva.es>
2026-02-10 19:03:41 +0100

    Chore: Estructura de paquetes y pom con JUnit

--- src/main/java/com/azahartech/eventdev/App.java
+++ src/main/java/com/azahartech/eventdev/App.java
@@ -1,3 +1,5 @@
+package com.azahartech.eventdev;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Esta va a ser la mejor App de la historia");
---
+++ src/test/java/com/azahartech/eventdev/AppTest.java
@@ -0,0 +1,9 @@
+package com.azahartech.eventdev;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+}
