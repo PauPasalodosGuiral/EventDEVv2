a3f77612af0074d7cf600520d89a68d3ab8698c3
Izan Moreno <izamorluc@alu.edu.gva.es>
2026-02-13 18:35:23 +0100

    Feat: Creación interfaz ProcesadorPago.

--- src/main/java/com/azahartech/eventdev/pagos/ProcesadorPago.java
+++ src/main/java/com/azahartech/eventdev/pagos/ProcesadorPago.java
@@ -10,7 +10,7 @@ public interface ProcesadorPago {
     /**
      * Ejecuta el procesamiento de un pago basado en una cantidad específica de entradas.
      *
-     * @param cant La cantidad total de entradas a cobrar. Debe ser un valor double positivo.
+     * @param cant La cantidad total de entradas a cobrar. Debe ser un valor double positivo
      * @return {@code true} si la transacción fue autorizada y completada con éxito;
      * {@code false} en caso de fondos insuficientes o error aleatorio.
      *
