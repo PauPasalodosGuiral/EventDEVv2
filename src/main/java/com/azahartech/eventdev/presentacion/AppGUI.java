1e2bb35574dbdd3c89a4dd8fec720235311fcd52
Pau Pasalodos <paupasalodos@gmail.com>
2026-03-18 16:49:11 +0100

    Semana 22 prt2 fase1

--- src/main/java/com/azahartech/eventdev/presentacion/AppGUI.java
+++ src/main/java/com/azahartech/eventdev/presentacion/AppGUI.java
@@ -10,6 +10,15 @@ public class AppGUI {
     public static void main(String[] args) {
 
         SwingUtilities.invokeLater(() -> {
+
+            try {
+                UIManager.setLookAndFeel(
+                        UIManager.getSystemLookAndFeelClassName()
+                );
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
             new VistaLogin().setVisible(true);
         });
     }
